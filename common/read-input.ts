a3b807d6f9e95457e07beb724a29fbcfc0d4f460
Adrian Bbannister <adrian.bannister@entelect.co.za>
2020-12-12 14:42:15 +0200

    Fix readInput for windows

--- common/read-input.ts
+++ common/read-input.ts
@@ -1,15 +1,15 @@
 import { readFileSync } from "fs";
-import { resolve } from "path";
+import { normalize, resolve, sep } from "path";
 
 export function readInput() {
   const stack = new Error().stack.split("\n");
-  const root = stack[2]
-    .replace(/ *at .* \((.*)\/.*\.(?:ts|js):\d+:\d+\)$/g, "$1")
-    .replace("/out", "");
-  const fileName = resolve(root, "input.txt");
+  const compiledPath = stack[2].replace(/at .*\((.*):\d+:\d+\)/, "$1");
+  const parts = resolve(compiledPath.trim()).split(sep);
+  parts.splice(parts.lastIndexOf("out"), 1);
+  const fileName = normalize(parts.join(sep) + "/../input.txt");
 
   const content = readFileSync(fileName, { encoding: "utf8" });
-  const lines = content.split("\n");
+  const lines = content.split(/(?:\r\n|\r|\n)/g);
   if (lines[lines.length - 1] === "") {
     lines.length--;
   }
