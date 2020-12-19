39b0cc27de153cb0e52cc59155b8fc97042befa3
Adrian Bbannister <adrian.bannister@entelect.co.za>
2020-12-19 17:37:12 +0200

    2015 day 23

---
+++ 2015/day-23/part1.ts
@@ -0,0 +1,48 @@
+import { readInput } from "../../common";
+
+function main() {
+  const input = readInput();
+
+  const r = {
+    a: 0,
+    b: 0,
+  };
+
+  let i = 0;
+  while (i < input.length) {
+    const [x, y, z] = input[i].split(" ");
+    switch (x) {
+      case "hlf":
+        r[y] /= 2;
+        break;
+      case "tpl":
+        r[y] *= 3;
+        break;
+      case "inc":
+        r[y]++;
+        break;
+      case "jmp":
+        i += Number(y) - 1;
+        break;
+      case "jie":
+        if (r[y[0]] % 2 === 0) {
+          i += Number(z) - 1;
+        }
+        break;
+      case "jio":
+        if (r[y[0]] === 1) {
+          i += Number(z) - 1;
+        }
+        break;
+
+      default:
+        throw new Error();
+    }
+
+    i++;
+  }
+
+  return r.b;
+}
+
+console.log(main());
