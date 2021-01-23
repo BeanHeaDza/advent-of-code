eeb96d0f8694a092afabc03064adc8c29884cb89
Adrian Bbannister <adrian.bannister@entelect.co.za>
2021-01-23 16:20:12 +0200

    2016 day 11

---
+++ 2016/day-11/part2.ts
@@ -0,0 +1,23 @@
+import { readInput } from "../../common";
+
+function parse(lines: string[]) {
+  const getGenerators = (line: string) =>
+    (line.match(/\w+ gen/g) || []).map((m) => m[0] + "g");
+  const getChips = (line: string) =>
+    (line.match(/\w+-comp/g) || []).map((m) => m[0] + "m");
+
+  return lines.map((l) => [...getGenerators(l), ...getChips(l)]);
+}
+
+function main() {
+  const input = parse(readInput());
+  input[0].push("eg", "em", "dg", "dm");
+
+  let answer = 3;
+  answer += (input[0].length - 2) * 6;
+  answer += input[1].length * 4;
+  answer += input[2].length * 2;
+  return answer;
+}
+
+console.log(main());
