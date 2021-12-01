786cf5d568f8c655a503ccf362c701fa4da39a04
Adrian <BeanHeaDza@users.noreply.github.com>
2021-12-01 17:20:30 +1100

    Update the new day template

--- new-day.ts
+++ new-day.ts
@@ -3,7 +3,17 @@ import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { resolve } from "path";
 import { createInterface, Interface } from "readline";
 
-// existsSync('')
+const SOURCE = `import { readInput } from "../../common";
+
+function main() {
+  const input = readInput();
+  return '';
+}
+
+console.log(main());
+`;
+
+
 function readLine(i: Interface) {
   return new Promise<string>((resolve, reject) => {
     const lineCb = (line: string) => {
@@ -32,8 +42,8 @@ async function main() {
   }
 
   writeFileSync(resolve(dayDir, "input.txt"), "");
-  writeFileSync(resolve(dayDir, "part1.ts"), "export {};");
-  writeFileSync(resolve(dayDir, "part2.ts"), "export {};");
+  writeFileSync(resolve(dayDir, "part1.ts"), SOURCE);
+  writeFileSync(resolve(dayDir, "part2.ts"), SOURCE);
 
   execSync("code " + resolve(dayDir, "part1.ts"));
   execSync("code " + resolve(dayDir, "input.txt"));
