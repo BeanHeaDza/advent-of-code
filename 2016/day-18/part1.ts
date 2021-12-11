4cd33457a405bf9dae951be6f27440729e98a54d
Adrian Bannister <bannister@canva.com>
2021-12-11 11:41:03 +1100

    2016 day 18

---
+++ 2016/day-18/part1.ts
@@ -0,0 +1,28 @@
+import { readInput } from "../../common";
+
+const TRAPS = new Set(["^^.", ".^^", "^..", "..^"]);
+
+function main() {
+  let row = readInput()[0];
+  let answer = row.split("").filter((c) => c === ".").length;
+  for (let r = 1; r < 40; r++) {
+    let newRow = "";
+    for (let i = 0; i < row.length; i++) {
+      const l = row[i - 1] || ".";
+      const c = row[i];
+      const r = row[i + 1] || ".";
+      const all = l + c + r;
+
+      if (TRAPS.has(all)) {
+        newRow += "^";
+      } else {
+        answer += 1;
+        newRow += ".";
+      }
+    }
+    row = newRow;
+  }
+  return answer;
+}
+
+console.log(main());
